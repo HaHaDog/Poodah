4f7c36b6d6741ade7ac8ed33f0390488e4c7aab2
xupingmao <xupingmao@gmail.com>
2013-12-20 14:56:39 +0800

    again

--- gen/com/poodah/R.java
+++ gen/com/poodah/R.java
@@ -115,17 +115,24 @@ public final class R {
         public static final int main=0x7f080000;
     }
     public static final class string {
+        public static final int OK=0x7f06000e;
         public static final int about=0x7f060009;
         public static final int action_settings=0x7f060001;
         public static final int app_name=0x7f060000;
         public static final int connect=0x7f060003;
+        public static final int connecting=0x7f060010;
+        public static final int gotoMain=0x7f060011;
         public static final int hello_world=0x7f060002;
         public static final int ip_label=0x7f060004;
+        public static final int pleaseInputIp=0x7f06000b;
+        public static final int pleaseInputPort=0x7f06000c;
         public static final int s_view_btn=0x7f060006;
         public static final int settings_name=0x7f060005;
         public static final int shock=0x7f060007;
         public static final int sound=0x7f060008;
         public static final int theme=0x7f06000a;
+        public static final int waitTitle=0x7f06000f;
+        public static final int warn=0x7f06000d;
     }
     public static final class style {
         /** 
